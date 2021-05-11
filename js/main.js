57ce83293d36cffb4f633b987e33335f719fdb69
bebi <dbuynaa01@gmail.com>
2021-05-12 01:46:07 +0800

    menu btn

--- js/main.js
+++ js/main.js
@@ -12,3 +12,26 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+document.getElementById("closeNav").addEventListener("click" ,function(){
+        if(document.getElementById("closeNav").style.display == ""){
+          closeNav();
+        }else{
+          openNav();
+        }
+});
+
+
+function openNav() {
+ document.getElementById("MenuBar").style.display="";
+ document.getElementById("main").style.left = "220px";
+ document.getElementById("main").style.width =  "calc(100% - 220px)";
+ document.getElementById("closeNav").style.display = "";
+}
+
+
+function closeNav() {
+  document.getElementById("main").style.left = "0%";
+  document.getElementById("main").style.width = "100%";
+ document.getElementById("MenuBar").style.display = "none";
+ document.getElementById("closeNav").style.display = "block"
+}
